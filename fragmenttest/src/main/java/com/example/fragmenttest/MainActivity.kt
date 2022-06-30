52c294f8424092ce52779c295809294edea4841b
bryan <zr_bryan@163.com>
2022-06-30 10:44:07 +0800

    fragment声明周期

--- fragmenttest/src/main/java/com/example/fragmenttest/MainActivity.kt
+++ fragmenttest/src/main/java/com/example/fragmenttest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_left_fragement.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +14,27 @@ class MainActivity : AppCompatActivity() {
             replaceFragment(AnotherRightFragment())
         }
         replaceFragment(Right_Fragement())
+
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.leftFrag)
+        val value = leftFrag as Left_Fragement
+        val activity = value.activity
     }
 
+    //将fragment添加到返回栈中,可以返回当前fragment
     private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val transaction = fragmentManager.beginTransaction()
         transaction.replace(R.id.rightLayout, fragment)
+        transaction.addToBackStack(null)
         transaction.commit()
     }
+    /* private fun replaceFragment(fragment: Fragment) {
+         val fragmentManager = supportFragmentManager
+         val transaction = fragmentManager.beginTransaction()
+         transaction.replace(R.id.rightLayout, fragment)
+         transaction.commit()
+     }*/
+
+
 }
